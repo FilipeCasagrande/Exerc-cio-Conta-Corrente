fffec463a6d06ec9c3e80d54dc37f6590b4af26f
Filipe <Filipecasagrande1@gmail.com>
2025-04-24 20:29:47 -0300

    Implementado Movimentação

--- Exercício Conta Corrente/ContaCorrente.cs
+++ Exercício Conta Corrente/ContaCorrente.cs
@@ -1,26 +1,42 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿
+using Exercício_Conta_Corrente;
 
-namespace Exercício_Conta_Corrente
+namespace ContaCorrente.ConsoleApp
 {
+
     public class ContaCorrente
     {
-        public decimal saldo;
         public int numero;
+        public decimal saldo;
         public decimal limite;
-
+        public Movimentacao[] movimentacoes;
+        int contadorMovimentacoes = 0;
 
         public void Sacar(decimal quantia)
         {
-            if (quantia < saldo + limite)
+            if (quantia <= saldo + limite)
             {
                 saldo -= quantia;
-            }
 
+                Movimentacao novaMovimentacao = new Movimentacao();
+                novaMovimentacao.valor = quantia;
+                novaMovimentacao.tipo = "Débito";
+
+                movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+                contadorMovimentacoes++;
+            }
         }
 
         public void Depositar(decimal quantia)
         {
             saldo += quantia;
+
+            Movimentacao novaMovimentacao = new Movimentacao();
+            novaMovimentacao.valor = quantia;
+            novaMovimentacao.tipo = "Crédito";
+
+            movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+            contadorMovimentacoes++;
         }
 
         public void TransferirPara(ContaCorrente destinatario, decimal quantia)
@@ -28,6 +44,25 @@ namespace Exercício_Conta_Corrente
             destinatario.Depositar(quantia);
 
             this.Sacar(quantia);
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("Extrato da Conta #" + this.numero);
+
+            Console.WriteLine("Saldo: " + this.saldo);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                Movimentacao movimentacaoAtual = movimentacoes[i];
+
+                if (movimentacaoAtual != null)
+                {
+                    Console.WriteLine(movimentacaoAtual.LerMovimentacao());
+                }
+            }
         }
     }
 }
